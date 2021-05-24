f0cec73a2699f864a2fbe38dd611255a5a7b184b
JinbaeByeon <quswlsqo@gmail.com>
2021-05-24 11:05:52 +0900

    StageMap 파일읽기

--- CookieRun/app/src/main/java/kr/ac/kpu/game/s2015182013/cookierun/game/StageMap.java
+++ CookieRun/app/src/main/java/kr/ac/kpu/game/s2015182013/cookierun/game/StageMap.java
@@ -1,8 +1,13 @@
 package kr.ac.kpu.game.s2015182013.cookierun.game;
 
+import android.content.res.AssetManager;
 import android.graphics.Canvas;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -12,7 +17,33 @@ import kr.ac.kpu.game.s2015182013.cookierun.framework.view.GameView;
 
 public class StageMap implements GameObject {
     private static final String TAG = StageMap.class.getSimpleName();
+    private final ArrayList<String> lines= new ArrayList<String>();
+    private int columns;
+    private int raws;
 
+    public StageMap(String filename){
+        AssetManager assets = GameView.view.getContext().getAssets();
+        try {
+            InputStream is = assets.open(filename);
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader reader = new BufferedReader(isr);
+            String header = reader.readLine();
+            String[] comps = header.split(" ");
+            columns = Integer.parseInt(comps[0]);
+            raws = Integer.parseInt(comps[1]);
+            Log.d(TAG,"Col = " + columns + ", Raw = " + raws);
+            while (true){
+                String line = reader.readLine();
+                if(line==null){
+                    break;
+                }
+                Log.d(TAG,"Line = " + line);
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void update() {
         MainGame game = (MainGame) BaseGame.get();
